19955a64b00b0b5dc86ce7f180afdcac8e179011
An Vo <albert@wthapps.com>
2017-09-11 14:59:27 +0700

    Add jquery support

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  # get 'orders/create'
-
-  # get 'orders/update'
-
-  # get 'orders/destroy'
-
-	# get 'order_items/create'
-
-	# get 'order_items/update'
-
-	# get 'order_items/destroy'
-
-	# get 'carts/show'
-
 	resource :cart, only: [:show]
 	resources :orders, only: [:create, :update, :destroy] do
 		resources :order_items, only: [:create, :update, :destroy]
